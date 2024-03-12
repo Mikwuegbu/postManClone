19c703fbf5d5c6194224c7fe319812c3076a5727
Mikwuegbu <mstephen130@yahoo.com>
2024-03-12 14:15:48 +0100

    switched to tailwind

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,4 +1,15 @@
+import Button from './Button';
+
 const Navbar = () => {
-	return <></>;
+	return (
+		<>
+			<div>search</div>
+			<div>
+				<Button value="Contact Sales" />
+				<Button value="Sign in" />
+				<Button value="Sign Up for Free" />
+			</div>
+		</>
+	);
 };
 export default Navbar;
