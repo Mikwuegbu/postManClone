dec02b0e4e4f06f6232e30b444c0bae2b2a17684
Mikwuegbu <mstephen130@yahoo.com>
2024-03-13 12:49:54 +0100

    mobile sign in

--- src/HeroBar.jsx
+++ src/HeroBar.jsx
@@ -1,10 +1,9 @@
-import { useState } from 'react';
 import { ReactTyped } from 'react-typed';
 
 const HeroBar = () => {
 	return (
 		<>
-			<div className="">
+			<div className="w-4/6 ml-10 pt-20">
 				<div className="text-4xl font-semibold font-mono">
 					<ReactTyped
 						strings={[
@@ -26,17 +25,19 @@ const HeroBar = () => {
 				<div className="text-3xl font-semibold tracking-tightfont-mono">
 					APIs together
 				</div>
-				<div className="text-base font-normal">
-					Over 30 million developers use Postman. Get started by signing up or
-					downloading the desktop app.
+				<div className="text-base font-normal pt-2">
+					Over 30 million developers use Postman. Get started by <br />
+					signing up or downloading the desktop app.
 				</div>
-				<div className="flex flex-col ">
+				<div className="flex flex-col  space-y-6 py-8">
 					<input
 						type="email"
 						placeholder="mstephen130@example.com"
-						className="border border-orange-500 px-2 py-2 text-sm"
+						className="border border-orange-500 px-2 py-1.5 text-sm rounded-md"
 					/>
-					<button>Sign up for Free</button>
+					<button className="text-white font-bold bg-orange-500 hover:bg-orange-600 py-2 px-3.5 rounded-md text-sm">
+						Sign up for Free
+					</button>
 				</div>
 			</div>
 		</>
