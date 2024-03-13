786ae914cdf633c74bdb5ec2f91294457c5e8a82
Mikwuegbu <mstephen130@yahoo.com>
2024-03-13 07:08:24 +0100

    working on responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
 import Navbar from './Navbar';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
 
 function App() {
 	return (
@@ -9,3 +13,4 @@ function App() {
 }
 
 export default App;
+library.add(fab, fas, far);
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,53 +1,15 @@
+import logo from './assets/pm-logo.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 const Navbar = () => {
 	return (
-		<nav className="flex py-2">
-			<div className="flex">
-				<h1>Logo</h1>
-				<ul className="flex space-x-7 py-1.5 font-semibold text-md tracking-tighter text-black ">
-					<li>
-						<a href="#" className="opacity-70 hover:opacity-100">
-							Product
-						</a>
-					</li>
-					<li>
-						<a href="#" className="opacity-70 hover:opacity-100">
-							Pricing
-						</a>
-					</li>
-					<li>
-						<a href="#" className="opacity-70 hover:opacity-100">
-							Enterprise
-						</a>
-					</li>
-					<li>
-						<a href="#" className="opacity-70 hover:opacity-100">
-							Resources and Support
-						</a>
-					</li>
-
-					<li>
-						<a href="#" className="opacity-70 hover:opacity-100">
-							Public API Network
-						</a>
-					</li>
-				</ul>
+		<nav className="flex py-2 px-3 justify-between">
+			<div>
+				<img src={logo} alt="" className="h-8 w-8 " />
 			</div>
-			<div className="px-2">
-				<input
-					placeholder="Search Postman"
-					className="bg-gray-300 py-1.5 rounded-md px-2 bg-opacity-30 border border-slate-200 hover:border hover:border-slate-400 text-sm font-thin"
-				/>
-			</div>
-			<div className="space-x-2">
-				<button className="border px-5 py-1.5 rounded-md font-normal text-sm hover:border-black hover:border-opacity-40">
-					Contact Sales
-				</button>
-				<button className="border px-5 py-1.5 rounded-md font-normal text-sm hover:border-black hover:border-opacity-40">
-					Sign in
-				</button>
-				<button className="border bg-orange-500 hover:bg-orange-600 text-white px-5 py-1.5 rounded-md font-semibold text-sm">
-					Sign Up for Free
-				</button>
+			<div className="space-x-6 px-3 py-2">
+				<FontAwesomeIcon icon="fa-solid fa-magnifying-glass" />
+				<FontAwesomeIcon icon="fa-solid fa-bars" size="lg" />
 			</div>
 		</nav>
 	);
